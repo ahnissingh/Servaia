8e8a385e7da1f5099f49a34edd6a61e81489d291
Ahnis <ahnisaneja@gmail.com>
2025-04-10 11:57:17 +0530

    Initialised MongoDB,VirtualThreads,OpenAIAPI,JWT Secret/Expiration(24hr)

---
+++ src/main/java/com/ahnis/servaia/ServaiaApplication.java
@@ -0,0 +1,13 @@
+package com.ahnis.servaia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServaiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServaiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahnis/servaia/ServaiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahnis.servaia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServaiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
